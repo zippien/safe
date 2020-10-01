c238eccb1f9732ef99c367e0218a7799afbb06f5
zippien <arulblood@gmail.com>
2020-10-01 16:03:28 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = "http://anikartika.com/safelink.html?url=" + encryptedUrl;
+a_to_ve[i].href = "http://anikartika.com/safelink/?url=" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
